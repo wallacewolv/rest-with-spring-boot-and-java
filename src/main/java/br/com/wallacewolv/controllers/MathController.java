ff7b5314814070cf15e5410fb53c0c4f34ede2c1
Wallace W <wallace_wolv@hotmail.com>
2025-07-30 00:59:56 -0300

    feat: (S1-A40) - [DESAFIO] Refatore o Código da Aplicação Separando os Conceitos

--- src/main/java/br/com/wallacewolv/controllers/MathController.java
+++ src/main/java/br/com/wallacewolv/controllers/MathController.java
@@ -1,6 +1,8 @@
 package br.com.wallacewolv.controllers;
 
 import br.com.wallacewolv.exception.UnsupportedMathOperationException;
+import br.com.wallacewolv.services.MathService;
+import br.com.wallacewolv.utils.NumberConverter;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,87 +11,79 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/math")
 public class MathController {
 
+    private final MathService mathService;
+
+    public MathController(MathService mathService) {
+        this.mathService = mathService;
+    }
+
     //http://localhost:8080/math/sum/3/5 - somatória
     @RequestMapping("sum/{numberOne}/{numberTwo}")
     public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo))
             throw new UnsupportedMathOperationException("Please set a numeric value!");
 
-        Double num1 = convertToDouble(numberOne);
-        Double num2 = convertToDouble(numberTwo);
+        Double num1 = NumberConverter.convertToDouble(numberOne);
+        Double num2 = NumberConverter.convertToDouble(numberTwo);
 
-        return num1 + num2;
+        return mathService.sum(num1, num2);
     }
 
     //http://localhost:8080/math/subtraction/3/5 - subtração
     @RequestMapping("subtraction/{numberOne}/{numberTwo}")
     public Double subtraction(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo))
             throw new UnsupportedMathOperationException("Please set a numeric value!");
 
-        Double num1 = convertToDouble(numberOne);
-        Double num2 = convertToDouble(numberTwo);
+        Double num1 = NumberConverter.convertToDouble(numberOne);
+        Double num2 = NumberConverter.convertToDouble(numberTwo);
 
-        return num1 - num2;
+        return mathService.subtraction(num1, num2);
     }
 
     //http://localhost:8080/math/multiplication/3/5 - multiplicação
     @RequestMapping("multiplication/{numberOne}/{numberTwo}")
     public Double multiplication(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo))
             throw new UnsupportedMathOperationException("Please set a numeric value!");
 
-        Double num1 = convertToDouble(numberOne);
-        Double num2 = convertToDouble(numberTwo);
+        Double num1 = NumberConverter.convertToDouble(numberOne);
+        Double num2 = NumberConverter.convertToDouble(numberTwo);
 
-        return num1 * num2;
+        return mathService.multiplication(num1, num2);
     }
 
     //http://localhost:8080/math/division/3/5 - divisão
     @RequestMapping("division/{numberOne}/{numberTwo}")
     public Double division(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo))
             throw new UnsupportedMathOperationException("Please set a numeric value!");
 
-        Double num1 = convertToDouble(numberOne);
-        Double num2 = convertToDouble(numberTwo);
+        Double num1 = NumberConverter.convertToDouble(numberOne);
+        Double num2 = NumberConverter.convertToDouble(numberTwo);
 
-        return num1 / num2;
+        return mathService.division(num1, num2);
     }
 
     //http://localhost:8080/math/mean/3/5 - média
     @RequestMapping("mean/{numberOne}/{numberTwo}")
     public Double mean(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo))
             throw new UnsupportedMathOperationException("Please set a numeric value!");
 
-        Double num1 = convertToDouble(numberOne);
-        Double num2 = convertToDouble(numberTwo);
+        Double num1 = NumberConverter.convertToDouble(numberOne);
+        Double num2 = NumberConverter.convertToDouble(numberTwo);
 
-        return (num1 + num2) / 2;
+        return mathService.mean(num1, num2);
     }
 
     //http://localhost:8080/math/square-root/81 - raiz quadrada
     @RequestMapping("square-root/{number}")
     public Double squareRoot(@PathVariable("number") String number) throws Exception {
-        if (!isNumeric(number)) throw new UnsupportedMathOperationException("Please set a numeric value!");
-
-        Double num = convertToDouble(number);
-        return Math.sqrt(num);
-    }
-
-    private Double convertToDouble(String strNumber) throws UnsupportedMathOperationException {
-        if (strNumber == null || strNumber.isEmpty())
+        if (!NumberConverter.isNumeric(number))
             throw new UnsupportedMathOperationException("Please set a numeric value!");
 
-        String number = strNumber.replace(",", "."); // R$ 5,00 UDS 5.0
-        return Double.parseDouble(number);
-    }
-
-    private boolean isNumeric(String strNumber) {
-        if (strNumber == null || strNumber.isEmpty()) return false;
-
-        String number = strNumber.replace(",", "."); // R$ 5,00 UDS 5.0
-        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+        Double num = NumberConverter.convertToDouble(number);
+        return mathService.squareRoot(num);
     }
 }
