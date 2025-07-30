fd6523c8f46ec43b5f390e1d9e3b7b475abb7dc6
Wallace W <wallace_wolv@hotmail.com>
2025-07-30 01:43:53 -0300

    feat: (S6-A47) - Retornando Listas de Objetos em JSON

--- src/main/java/br/com/wallacewolv/controllers/PersonController.java
+++ src/main/java/br/com/wallacewolv/controllers/PersonController.java
@@ -9,12 +9,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/person")
 public class PersonController {
 
     @Autowired
     private PersonServices service;
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public List<Person> findAll() {
+        return service.findAll();
+    }
 
     @RequestMapping(value = "/{id}",
             method = RequestMethod.GET,
--- src/main/java/br/com/wallacewolv/services/PersonServices.java
+++ src/main/java/br/com/wallacewolv/services/PersonServices.java
@@ -3,6 +3,8 @@ package br.com.wallacewolv.services;
 import br.com.wallacewolv.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -12,6 +14,27 @@ public class PersonServices {
     private final AtomicLong counter = new AtomicLong();
 
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public List<Person> findAll() {
+        List<Person> persons = new ArrayList<Person>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+
+        return persons;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("FirstName " + i );
+        person.setLastName("LastName" + i);
+        person.setAddress("Some Address in Brasil");
+        person.setGender("Male");
+
+        return person;
+    }
 
     public Person findById(String id) {
         logger.info("Finding one Person!");
